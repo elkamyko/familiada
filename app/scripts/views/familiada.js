1b4871506cb38fc87cc371fa5d5c5e6d0a13f7f2
adriansiulik <asiulik@180hb.com>
2015-02-04 23:20:20 +0100

    up

--- app/scripts/views/familiada.js
+++ app/scripts/views/familiada.js
@@ -24,35 +24,11 @@ define([
         },
 		events: {
 			'click #familiada-start-button': 'start'
-//			'change .address-for-google': 'setModelAddressForGoogle',
-//			'keyup .address-for-google': 'setModelAddressForGoogle',
-//
-//			'blur .custom-lat': 'setModelCustomLocation',
-//			'change .custom-lat': 'setModelCustomLocation',
-//			'keyup .custom-lat': 'setModelCustomLocation',
-//
-//			'blur .custom-lng': 'setModelCustomLocation',
-//			'change .custom-lng': 'setModelCustomLocation',
-//			'keyup .custom-lng': 'setModelCustomLocation',
-//
-//			'click .geocoding-result li button': 'stopEventPropagation',
-//			'click .geocoding-result li input': 'stopEventPropagation',
-//			'click .geocoding-result li label': 'stopEventPropagation',
-//			'click .geocoding-result li': 'selectLocation',
-//
-//			'change .geocoding-result input[type="radio"]': 'locationSelected',
-//
-//			'click .save': 'saveLocation',
-//
-//			'click .geocoding-trigger': 'geocode',
-//
-//			'click .show-on-map': 'showOnMap',
-//
-//			'click .from-map': 'fromMap'
 
 		},
 		$scene: null,
 		initialize: function (opts) {
+
             this.$scene = $('#scene');
             this.$scene.append(this.$el);
             this.gameView = new GameView();
@@ -70,7 +46,9 @@ define([
             });
 
 		},
+
 		render: function () {
+
 			var template = this.getTemplate(),
                     data = {};
 			
@@ -80,6 +58,7 @@ define([
                 
 			this.$el.html(template(data));
 		},
+
         start: function () {
             this.gameView.render();
         }
@@ -87,5 +66,4 @@ define([
 	}, Backbone.Events);
 
 	return FamiliadaView;
-
 });
